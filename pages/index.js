9d36148df3db4cfa4141b1adad173346c37a9e43
yurilucena <yurikaicklucena@gmail.com>
2024-04-03 12:47:24 -0300

    add manifesto e pages

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function Home() {
+    return(
+        <div>
+            <h1>Bora Lol?</h1>
+            <a href="https://www.youtube.com/watch?v=4Lsbg4tq9-Q">Link Suspeito</a>
+        </div>
+    ) 
+}
+
+export default Home;

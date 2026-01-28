fcdbb5a514558a7fa5d9f75d0ec6a64f433bda84
Yuri Lucena <yurikaicklucena@gmail.com>
2026-01-28 17:43:00 -0300

    adiciona comandos para o prettier

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 function Home() {
-    return(
+    return (
         <div>
             <h1>Bora Lol?</h1>
-            <a href="https://www.youtube.com/watch?v=4Lsbg4tq9-Q">Link Suspeito</a>
+            <a href="https://www.youtube.com/watch?v=4Lsbg4tq9-Q">
+                Link Suspeito
+            </a>
         </div>
-    ) 
+    );
 }
 
 export default Home;
